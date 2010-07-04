dd13467ec99103e9275f381658140fe22d8a8e4d
Matt Anderson <Synse@users.noreply.github.com>
2010-07-04 05:44:06 +0000

    Updated for 3.3.5a

--- TTell.lua
+++ TTell.lua
@@ -1,52 +1,82 @@
--- binding variables
-BINDING_HEADER_TTELL = "TTell"
-BINDING_NAME_TELLTARGET = "Whisper Target"
+-- globals for bindings
+BINDING_HEADER_TTELL = "TTell";
+BINDING_NAME_TELLTARGET = "Whisper Target";
 
-function TTell_OnLoad(self)
-	-- register slash commands
-	SlashCmdList["TTELL_TELLTARGET"] = function(message)
-		TTell_TellTarget(message);
-	end
-	SLASH_TTELL_TELLTARGET1 = "/tt"
-	SLASH_TTELL_TELLTARGET2 = "/wt"
-
-	SlashCmdList["TTELL_RETELL"] = function(message)
-		TTell_ReTell(message);
-	end
-	SLASH_TTELL_RETELL1 = "/rt"
-
-	-- hook editBox OnSpacePressed
-	hooksecurefunc("ChatEdit_OnSpacePressed", TTell_OnSpacePressed);
-
-	-- register events
-	--self:RegisterEvent("ADDON_LOADED"); -- currently only used for debugging
+-- slash commands
+SlashCmdList["TTELL_TELLTARGET"] = function(message)
+	TTell_TellTarget(message);
 end
+SLASH_TTELL_TELLTARGET1 = "/tt";
+SLASH_TTELL_TELLTARGET2 = "/wt";
 
-function TTell_OnEvent(self, event, ...)
-	if (event == "ADDON_LOADED") then
-		local addonName = select(1, ...);
-		if (addonName == "TTell") then
-			TTell_AddOnMessage(" loaded.");
-		end
-		return
-	end
+SlashCmdList["TTELL_RETELL"] = function(message)
+	TTell_ReTell(message);
 end
+SLASH_TTELL_RETELL1 = "/rt";
+SLASH_TTELL_RETELL2 = "/rtt";
 
-function TTell_OnSpacePressed()
-	local editBoxText = strlower(DEFAULT_CHAT_FRAME.editBox:GetText());
+local function TTell_OnSpacePressed(editBox)
+	local editBoxText = strlower(editBox:GetText());
 
-	-- prevent unnecessary matching attempts
-	if (editBoxText:sub(1, 1) ~= "/"  or editBoxText:len() > 4) then
-		return
+	-- optimization
+	if (editBoxText:sub(1, 1) ~= "/"  or editBoxText:len() > 5) then
+		return;
 	end
 
-	-- search for a match
+	-- match commands
 	if (editBoxText:match("^/[tw]t $")) then
 		TTell_TellTarget();
-	elseif (editBoxText:match("^/rt $")) then
+	elseif (editBoxText:match("^/rt[t]? $")) then
 		TTell_ReTell();
+	end
+end
+
+-- hook ChatEdit_OnSpacePressed function
+hooksecurefunc("ChatEdit_OnSpacePressed", TTell_OnSpacePressed);
+
+local function TTell_GetSelectedChatFrame()
+	if (SELECTED_CHAT_FRAME.editBox:IsShown()) then
+		return SELECTED_CHAT_FRAME;
+	end
+
+	-- floating SELECTED_CHAT_FRAME bug
+	for i=0, NUM_CHAT_WINDOWS do
+		local chatFrame = getglobal("ChatFrame" .. i);
+		if (chatFrame ~= nil and chatFrame.editBox:IsShown()) then
+			--print("SELECTED_CHAT_FRAME should be ChatFrame" .. i .. " was ChatFrame" .. SELECTED_CHAT_FRAME:GetID());
+			return chatFrame;
+		end
+	end
+end
+
+local function TTell_AddOnMessage(message)
+	local chatFrame = TTell_GetSelectedChatFrame();
+	chatFrame:AddMessage("|c440099ff[TTell]|cff00ffff " .. message);
+end
+
+local function TTell_GetTargetName()
+	local name, realm = UnitName("target");
+	if (realm ~= nil and realm:len() > 0) then
+		--local realm = realm:gsub("%s", "");
+		return string.join("-", name, realm);
+	end
+	return name;
+end
+
+local function TTell_NewMessage(tellTarget, message)
+	if (message == nil) then
+		local editBox = TTell_GetSelectedChatFrame().editBox;
+
+		editBox:SetAttribute("tellTarget", tellTarget);
+		editBox:SetAttribute("chatType", "WHISPER");
+		editBox.setText = 1;
+		editBox.text = "";
+		editBox:SetFocus();
+
+		ChatEdit_UpdateHeader(editBox);
 	else
-		return
+		SendChatMessage(message, "WHISPER", GetDefaultLanguage("player"), tellTarget);
+		ChatEdit_SetLastToldTarget(tellTarget);
 	end
 end
 
@@ -66,33 +96,4 @@ function TTell_ReTell(message)
 	else
 		TTell_AddOnMessage("no previous tell target.");
 	end
-end
-
-function TTell_NewMessage(tellTarget, message)
-	if (message == nil) then
-		DEFAULT_CHAT_FRAME.editBox:Hide();
-		DEFAULT_CHAT_FRAME.editBox:SetAttribute("chatType", "WHISPER");
-		DEFAULT_CHAT_FRAME.editBox:SetAttribute("tellTarget", tellTarget);
-		if (not DEFAULT_CHAT_FRAME.editBox:IsShown()) then
-			DEFAULT_CHAT_FRAME.editBox:Show();
-			DEFAULT_CHAT_FRAME.editBox.setText = 1;
-			DEFAULT_CHAT_FRAME.editBox.text = "";
-		end
-	else
-		SendChatMessage(message, "WHISPER", GetDefaultLanguage("player"), tellTarget);
-		ChatEdit_SetLastToldTarget(tellTarget);
-	end
-end
-
-function TTell_GetTargetName()
-	local name, realm = UnitName("target");
-	if (realm ~= nil and realm:len() > 0) then
-		local realm = realm:gsub("%s", "");
-		return string.join("-", name, realm);
-	end
-	return name;
-end
-
-function TTell_AddOnMessage(message)
-	DEFAULT_CHAT_FRAME:AddMessage("|c440099ff[TTell]|cff00ffff " .. message);
 end
